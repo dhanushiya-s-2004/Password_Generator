6b8173ae9c7ff8c29321eca06cc2dbd5cf8eebfd
Dhanushiya <dhanushiya1214@gmail.com>
2026-06-10 15:47:53 +0530

    First commit - password generator

---
+++ password_generator.py
@@ -0,0 +1,19 @@
+import random
+import string
+
+print("🔐 Password Generator")
+
+length = int(input("Enter password length: "))
+
+letters = string.ascii_letters
+numbers = string.digits
+symbols = string.punctuation
+
+all_chars = letters + numbers + symbols
+
+password = ""
+
+for i in range(length):
+    password += random.choice(all_chars)
+
+print("Generated Password:", password)
